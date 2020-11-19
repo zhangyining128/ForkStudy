c845ed4bc2d5e19975cb53e201d20bb50ab7e3a4
zhangyining128 <zhangyining128@outlook.com>
2020-11-19 16:47:15 +0800

    Create ForkStudy project

---
+++ src/ForkStudy/ForkStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForkStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
